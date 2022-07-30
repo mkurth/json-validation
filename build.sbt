c8933ed5a3c28a713a4601f8186341af4b436469
Michael Kurth <michael.kurth@perseus.de>
2022-07-30 16:44:22 +0200

    Added circe to drop null values from json, implemented validation

--- build.sbt
+++ build.sbt
@@ -8,6 +8,7 @@ lazy val domain = (project in file("domain"))
     name                := "json-schema-validator",
     libraryDependencies += "org.typelevel"             %% "cats-effect"           % "3.3.14",
     libraryDependencies += "org.scalatest"             %% "scalatest"             % "3.2.12" % "test",
+    libraryDependencies += "io.circe"                  %% "circe-parser"          % "0.14.2",
     libraryDependencies += "com.github.java-json-tools" % "json-schema-validator" % "2.2.14"
   )
 
--- domain/src/main/scala/com/snowplow/json/JsonSchemaValidation.scala
+++ domain/src/main/scala/com/snowplow/json/JsonSchemaValidation.scala
@@ -1,18 +1,68 @@
 package com.snowplow.json
 
+import cats.Monad
 import cats.data.EitherT
-import com.snowplow.json.JsonSchemaRegistry.{LoadJsonSchema, PersistenceError}
+import com.fasterxml.jackson.databind.JsonNode
+import com.github.fge.jackson.JsonLoader
+import com.github.fge.jsonschema.core.report.ProcessingReport
+import com.github.fge.jsonschema.main.JsonSchemaFactory
+import com.snowplow.json.JsonSchemaRegistry._
+import io.circe.parser._
+import scala.jdk.CollectionConverters.IteratorHasAsScala
+import scala.util.Try
 
 object JsonSchemaValidation {
 
   sealed trait ValidationError
   case object InvalidJson                                                   extends ValidationError
   final case class JsonDoesNotMatchSchema(id: String, errors: List[String]) extends ValidationError
+  final case class GeneralValidationError(id: String, error: String)        extends ValidationError
+  final case class SchemaDoesNotExist(id: String)                           extends ValidationError
 
   final case class ValidJsonSchema(id: String)
 
-  def validateJsonSchema[F[_]](
-      loadJsonSchema: LoadJsonSchema[F]
-  )(id: String, jsonToValidate: String): EitherT[F, ValidationError, ValidJsonSchema] = ???
+  private lazy val validator = JsonSchemaFactory.byDefault().getValidator
 
+  def validateJsonSchema[F[_]: Monad](
+      loadJsonSchema: LoadJsonSchema[F]
+  )(id: String, jsonToValidate: String): EitherT[F, ValidationError, ValidJsonSchema] =
+    loadJsonSchema(id)
+      .leftMap {
+        case GeneralPersistenceError(_) => GeneralValidationError(id, "internal error, try again later")
+        case SchemaAlreadyExists(id)    => GeneralValidationError(id, "internal error")
+        case SchemaNotFound(id)         => SchemaDoesNotExist(id)
+      }
+      .flatMap { jsonSchema =>
+        EitherT.fromEither(for {
+          schema           <- loadSchemaAsJson(jsonSchema)
+          cleaned          <- cleanEmptyKeys(jsonToValidate)
+          toValidateAsJson <- loadAsJson(cleaned)
+          validationResult <- validateJsonWithSchema(id, schema, toValidateAsJson)
+          result           <- handleValidationResult(id, validationResult)
+        } yield result)
+      }
+
+  private def loadSchemaAsJson(jsonSchema: JsonSchema) =
+    tryOrEither(
+      () => JsonLoader.fromString(jsonSchema.body),
+      _ => GeneralValidationError(jsonSchema.id, "persisted schema is corrupt")
+    )
+
+  private def loadAsJson(jsonToValidate: String) =
+    tryOrEither(f = () => JsonLoader.fromString(jsonToValidate), onError = _ => InvalidJson)
+
+  private def cleanEmptyKeys(json: String) = parse(json).map(_.deepDropNullValues.toString()).left.map(_ => InvalidJson)
+
+  private def validateJsonWithSchema(id: String, schema: JsonNode, toValidateAsJson: JsonNode) =
+    tryOrEither(
+      () => validator.validate(schema, toValidateAsJson),
+      e => GeneralValidationError(id, e.getMessage)
+    )
+
+  private def handleValidationResult(id: String, validationResult: ProcessingReport) =
+    if (validationResult.isSuccess) Right(ValidJsonSchema(id))
+    else Left(JsonDoesNotMatchSchema(id, validationResult.iterator().asScala.map(_.getMessage).toList))
+
+  private def tryOrEither[A](f: () => A, onError: Throwable => ValidationError): Either[ValidationError, A] =
+    Try(f()).toEither.left.map(onError)
 }
